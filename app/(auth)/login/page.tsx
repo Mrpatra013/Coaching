2f41a31275ac6216fdd351d8153f8a6c48df1bd7
mrpatra08 <patrasahil013@gmail.com>
2026-03-11 23:55:25 +0530

    creating the auth pages

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { GithubIcon } from "lucide-react";
+
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Welcome Back!</CardTitle>
+        <CardDescription>Login to your account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Button className="w-full" variant="outline">
+        <GithubIcon className="mr-2 h-4 w-4" />
+        Login with GitHub
+        </Button>
+
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+          <span className="relative z-10 bg-card px-2 text-muted-foreground">or continue with</span>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from "@/components/ui/themeToggle";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold text-center text-red-500">Hello World</h1>
+      <ModeToggle />
     </div>
   );
 }
