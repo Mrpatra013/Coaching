bfc8be3ec6f346b8a8f342e38ea48d3746b36764
mrpatra08 <patrasahil013@gmail.com>
2026-03-16 12:56:18 +0530

    installed better-auth , neondb

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,33 @@
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeftIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link"
+import logo from "@/public/logo.png"
+
 export default function Authlayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center">
+      <Link href="/" className={buttonVariants({
+        variant: "outline", 
+        className: "absolute top-4 left-4"
+      })} >
+        <ArrowLeftIcon className="size-4" />Back
+      </Link>
+
       <div className="flex w-full max-w-sm flex-col gap-6">
+      <Link href="/" className="flex items-center gap-2 self-center font-bold ">
+      <Image src={logo} alt="logo" width={32} height={32} />
+      Pathshalaa .
+      </Link>
       {children}
+
+      <div className="text-balance text-center text-xs text-muted-foreground">
+        By clicking continue, you agree to our <span className=" hover:underline text-blue-600">Terms of service </span>
+        and <span className=" hover:underline text-blue-600">Privacy policy</span>
+      </div>
     </div>
     </div>
   );
+
+
 }
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { GithubIcon } from "lucide-react";
 
 export default function Login() {
@@ -9,7 +11,7 @@ export default function Login() {
         <CardTitle>Welcome Back!</CardTitle>
         <CardDescription>Login to your account</CardDescription>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex flex-col gap-4">
         <Button className="w-full" variant="outline">
         <GithubIcon className="mr-2 h-4 w-4" />
         Login with GitHub
@@ -17,6 +19,17 @@ export default function Login() {
 
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
           <span className="relative z-10 bg-card px-2 text-muted-foreground">or continue with</span>
+        </div>
+
+        <div className="grid gap-3 ">
+          <div className="grid gap-2 ">
+            <Label htmlFor="email">Email </Label>
+            <Input id="email" type="email" placeholder="abc@gmail.com" />
+          </div>
+
+          <Button>
+            Continue with Email
+          </Button>
         </div>
       </CardContent>
     </Card>
