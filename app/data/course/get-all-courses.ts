46cdb7b4dfff4c5848e8aeefa0c39e94af40a7cc
mrpatra08 <patrasahil013@gmail.com>
2026-04-20 16:19:08 +0530

    public Course route

---
+++ app/data/course/get-all-courses.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/lib/db";
+
+export async function getAllCourses(){
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  try {
+    const data = await prisma.course.findMany({
+      where:{
+        status : "PUBLISHED",
+      },
+      orderBy:{
+        createdAt: "desc",
+      },
+      select:{
+        title: true,
+        price:true,
+        smallDescription: true,
+        category: true,
+        slug: true,
+        fileKey: true,
+        id:true,
+        level:true,
+        duration:true,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch courses:", error);
+    return [];
+  }
+}
+
+export type PublicCourse = Awaited<ReturnType<typeof getAllCourses>>[0];
