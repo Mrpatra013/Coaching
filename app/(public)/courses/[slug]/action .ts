dfe3b5c05b73248c880b347d4f2e196539ec2682
mrpatra08 <patrasahil013@gmail.com>
2026-04-22 11:27:34 +0530

    started with payment gateway

---
+++ app/(public)/courses/[slug]/action .ts
@@ -0,0 +1,46 @@
+"use server"
+
+import { requireUser } from "@/app/data/user/require-user";
+import { prisma } from "@/lib/db";
+import { ApiResponse } from "@/lib/type";
+
+export async function enrollInCourseAction(courseId:string):Promise<ApiResponse>{
+  const user = await requireUser();
+  try {
+    
+    const course = await prisma.course.findUnique({
+      where:{
+        id:courseId
+      },
+      select:{
+        id:true,
+        title:true,
+        price:true,
+        slug:true,
+      }
+    });
+
+    if(!course){
+      return {
+        status:"error",
+        message:"Course not found",
+      };
+    }
+
+    const userWithRazorCustomerId = await prisma.user.findUnique({
+      where:{
+        id : user.id,
+      },
+      select:{
+        razorCustomerId : true,
+        
+      },
+    });
+
+  } catch{
+    return{
+      status:"error",
+      message:"Failed to enroll in Course ."
+    };
+  }
+}
